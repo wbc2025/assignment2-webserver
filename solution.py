85560f3e4044bc9e89cd6a09636119df9017f18d
W. Brett Christiansen <wbchristiansen@gmail.com>
2022-02-17 20:46:48 -0600

    First attempt

--- solution.py
+++ solution.py
@@ -1,4 +1,6 @@
 # import socket module
+from http import server
+from multiprocessing import connection
 from socket import *
 # In order to terminate the program
 import sys
@@ -8,25 +10,28 @@ def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
+  
   #Fill in start
-
+  serverSocket.listen(5)
   #Fill in end
 
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.readlines()   # read file
         
         #Send one HTTP header line into socket.
         #Fill in start
-
+        connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+        connectionSocket.send('Content-Type: text/html\r\n'.encode())
+        connectionSocket.send("\r\n".encode())
         #Fill in end
 
         #Send the content of the requested file to the client
@@ -38,13 +43,14 @@ def webServer(port=13331):
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-
+        connectionSocket.send('HTTP/1.1 404 NOT FOUND\r\n'.encode())
+        
         #Fill in end
 
 
         #Close client socket
         #Fill in start
-
+        connectionSocket.close()
         #Fill in end
 
     except (ConnectionResetError, BrokenPipeError):
